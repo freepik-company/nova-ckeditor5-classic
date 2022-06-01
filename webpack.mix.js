5b568bb53c27b9802172892b08631a5af13d1ee3
Juan Carlos Muñoz Cabrera <jcmunoz@freepik.com>
2022-06-01 11:05:06 +0200

    nova 4

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,5 +7,4 @@ mix
     .setPublicPath('dist')
     .js('resources/js/field.js', 'js')
     .vue({ version: 3 })
-    .sass('resources/sass/field.scss', 'css')
     .nova('numaxlab/nova-ckeditor5-classic')
